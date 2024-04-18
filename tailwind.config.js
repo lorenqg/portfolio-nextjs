d85dbaf0800948a8572cbbc660a3c2885a60b037
Loren Q <lorenquiceno@gmail.com>
2024-04-18 15:40:12 -0500

    Inicializar repositorio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
+import colors from 'tailwindcss/colors'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        base: '#0f0217',
+        primary: { ...colors.blue, DEFAULT: '#2F3BA6' },
+        secondary: { ...colors.fuchsia, DEFAULT: '#871E4D' }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
